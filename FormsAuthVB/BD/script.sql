12dd35cf45eac721fd5547746715b0213e87252a
aacoxc <aacoxc@cotemar.com.mx>
2020-06-29 20:46:27 -0500

    Se agrega script de bd demo

---
+++ FormsAuthVB/BD/script.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE pruebas;
+GO
+USE pruebas;
+GO
+CREATE TABLE [dbo].[Users] (
+   [uname] [varchar] (15) NOT NULL primary key,
+   [Pwd] [varchar] (25) NOT NULL ,
+   [userRole] [varchar] (25) NOT NULL ,
+)
+GO
+INSERT INTO Users VALUES ('admin', '1234', 'A'), ('prov', '1234', 'P');
+GO
+SELECT * FROM Users;
